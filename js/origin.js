b7e2c05cf338b96bed12e6b98b3759e598a81271
くまくま <as.if.in.a.dream0805@gmail.com>
2020-05-02 21:46:56 +0900

    フェードインの調整

--- js/origin.js
+++ js/origin.js
@@ -60,33 +60,45 @@ $(function () {
   });
 });
 
-// 飾り要素のフェードイン
+// // 飾り要素のフェードイン
+// $(function () {
+//   var effect_pos = 1400; // 画面下からどの位置でフェードさせるか(px)
+//   var effect_move = 50; // どのぐらい要素を動かすか(px)
+//   var effect_time = 600; // エフェクトの時間(ms) 1秒なら1000
+
+//   // フェードする前のcssを定義
+//   $('.fadein').css({
+//     opacity: 0,
+//     transform: 'translateY(' + effect_move + 'px)',
+//     transition: effect_time + 'ms'
+//   });
+
+//   // スクロールまたはロードするたびに実行
+//   $(window).on('scroll load', function () {
+//     var scroll_top = $(this).scrollTop();
+//     var scroll_btm = scroll_top + $(this).height();
+//     effect_pos = scroll_btm - effect_pos;
+
+//     // effect_posがthis_posを超えたとき、エフェクトが発動
+//     $('.fadein').each(function () {
+//       var this_pos = $(this).offset().top;
+//       if (effect_pos > this_pos) {
+//         $(this).css({
+//           opacity: 1,
+//           transform: 'translateY(0)'
+//         });
+//       }
+//     });
+//   });
+// });
 $(function () {
-  var effect_pos = 1400; // 画面下からどの位置でフェードさせるか(px)
-  var effect_move = 50; // どのぐらい要素を動かすか(px)
-  var effect_time = 600; // エフェクトの時間(ms) 1秒なら1000
-
-  // フェードする前のcssを定義
-  $('.fadein').css({
-    opacity: 0,
-    transform: 'translateY(' + effect_move + 'px)',
-    transition: effect_time + 'ms'
-  });
-
-  // スクロールまたはロードするたびに実行
-  $(window).on('scroll load', function () {
-    var scroll_top = $(this).scrollTop();
-    var scroll_btm = scroll_top + $(this).height();
-    effect_pos = scroll_btm - effect_pos;
-
-    // effect_posがthis_posを超えたとき、エフェクトが発動
+  $(window).scroll(function () {
     $('.fadein').each(function () {
-      var this_pos = $(this).offset().top;
-      if (effect_pos > this_pos) {
-        $(this).css({
-          opacity: 1,
-          transform: 'translateY(0)'
-        });
+      var position = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > position - windowHeight + 200) {
+        $(this).addClass('active');
       }
     });
   });
